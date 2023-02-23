b07d270890db83b33bb6b7b8ae0064d3888fab39
ynvYauneEnovore <yovanuxf@gmail.com>
2023-02-23 14:23:52 -0400

    actualizacion de iconos

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,8 @@ import '../scss/styles.scss'
 
 // Importar todo JSde boostrap
 import * as bootstrap from 'bootstrap'
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
