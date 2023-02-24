62880626bf87dea98b51939353b5cb875e69e64d
ynvYauneEnovore <yovanuxf@gmail.com>
2023-02-24 06:42:21 -0400

    dia -2

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,11 @@ import '../scss/styles.scss'
 // Importar todo JSde boostrap
 import * as bootstrap from 'bootstrap'
 
+// Importar todos los iconos
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
+
+// Importar los css externos 1 boton central
+import '../scss/boton.css'
